d7f30ad1419d4811e87683ea9445b2d75b1451df
Anonymous <none>
2015-03-10 09:50:35 -0700

    reversible percentages

--- dq9rng.py
+++ dq9rng.py
@@ -8,11 +8,21 @@ def rand():
     return seed >> 32
 
 
+def rand_with_seed(myseed):
+    myseed = (myseed * 0x5d588b656c078965) + 0x269ec3
+    myseed = myseed & 0xFFFFFFFFFFFFFFFF
+    return myseed
+
+
 def percentage(value):
     return value * 100.0 / (2 << 31)
 
 
-if __name__ == "__main__":
+def reverse_percentage(percentage):
+    return int(percentage * (2 << 31) / 100.0)
+
+
+def test_rand():
     seed = raw_input("Initial seed (in hex)? ")
     seed = int(seed, 0x10)
     while True:
@@ -20,3 +30,37 @@ if __name__ == "__main__":
         print "%x" % seed
         print "%s%%" % percentage(value)
         raw_input("... ")
+
+
+def rand_search(low, high, number=None, depth=None):
+    initial_seeds = range(0x100000)
+    chains = [[i] for i in initial_seeds]
+    successes = []
+    done = False
+    counter = 0
+    while True:
+        for c in chains:
+            prev = c[-1]
+            iterated = rand_with_seed(prev)
+            c.append(iterated)
+            if low <= (iterated >> 32) <= high:
+                successes.append(list(c))
+            if number is not None and len(successes) >= number:
+                done = True
+                break
+        counter += 1
+        if depth is not None and counter >= depth:
+            break
+        if done:
+            break
+    return successes
+
+if __name__ == "__main__":
+    low = reverse_percentage(0.95)
+    high = reverse_percentage(0.96)
+    print low, high
+    print rand_search(low, high, number=1)[0]
+    initials = rand_search(low, high, depth=1)
+    for i in initials:
+        print i, percentage(i[-1] >> 32)
+    print len(initials)
